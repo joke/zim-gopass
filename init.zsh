3f18088025026052b75b6a2438e303054e9bc33b
Joke de Buhr <joke@xckk.de>
2022-09-17 17:18:01 +0200

    feat: check dumpfile after completion update
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -4,5 +4,8 @@
   [[ -z $command ]] && return 1
 
   local compfile=$1/functions/_gopass
-  [[ ! -e $compfile || $compfile -ot $command ]] && $command completion zsh >| $compfile
+  if [[ ! -e $compfile || $compfile -ot $command ]];
+	  $command completion zsh >| $compfile
+    zimfw check-dumpfile
+  fi
 } ${0:h}
