3f2ca5f8b949df904e41a8ecfb678dcd80792a2f
Joke de Buhr <joke@xckk.de>
2023-03-12 16:18:22 +0100

    fix: new version detection message
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -6,6 +6,6 @@
   local compfile=$1/functions/_gopass
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command completion zsh >| $compfile
-    print -u2 -PR "* Detected a new version 'gopass'. Regenerated completions.
+    print -u2 -PR "* Detected a new version 'gopass'. Regenerated completions."
   fi
 } ${0:h}
